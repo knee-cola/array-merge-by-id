d995ca8d9f3ea3829ce03f7c49c5f917f824e2cf
knee-cola <nikola.derezic@gmail.com>
2017-12-13 12:57:29 +0100

    done testing the filter function

--- lib/array-filter.ts
+++ lib/array-filter.ts
@@ -1,4 +1,4 @@
-import { CompareBy, ComparerFn } from './lib-types';
+import { CompareBy, ComparerFn, Map } from './lib-types';
 import compileC from './comparer-compiler';
 
 /**
@@ -8,7 +8,7 @@ import compileC from './comparer-compiler';
  * @param key_values key values to be found
  * @param findFirstOnly should only the first matched element be returned (defaults to `false`)
  */
-const filterByKeys = <T>(aSearch:Array<T>, key_columns:CompareBy, key_values:Array<string>, findFirstOnly:boolean=false):Array<T> => {
+const filterByKeys = <T>(aSearch:Array<T>, key_columns:CompareBy, key_values:Map=null, findFirstOnly:boolean=false):Array<T> => {
 
     let fnComparer:ComparerFn = compileC(key_columns),
         result:Array<T> = [];
@@ -26,33 +26,4 @@ const filterByKeys = <T>(aSearch:Array<T>, key_columns:CompareBy, key_values:Arr
     return(result);
 }
 
-/**
- * Returns the first matched element of the given type
- * @param aSearch array to be searched
- * @param key_columns key column names
- * @param key_values key values to be matched
- */
-const findFirstById = <T>(aSearch:Array<T>, key_columns:CompareBy, key_values:Array<string>):T => {
-    return(filterByKeys(aSearch, key_columns, key_values, true)[0]);
-}
-
-/**
- * Returns index of first element in the given array which is matched by provided key values
- * @param aSearch array to be searched
- * @param key_columns key column names
- * @param key_values key column values
- */
-const indexOf = <T>(aSearch:Array<T>, key_columns:CompareBy, key_values:Array<string>):number => {
-    
-    let fnComparer = compileC(key_columns);
-
-    for(var i=0,max=aSearch.length;i<max;i++) {
-        if(fnComparer(aSearch[i], key_values)===0) {
-            return(i);
-        }
-    }
-
-    return(-1);
-}
-
-export { filterByKeys, findFirstById, indexOf }
+export default filterByKeys;
---
+++ test/array-filter.spec.ts
@@ -0,0 +1,57 @@
+import { originalMainParentArr, originalSubparentArr, originalArr } from './test-data';
+import filterByKeys from '../lib/array-filter';
+
+describe('filterByKeys', () => {
+
+    let testData:Array<any>;
+
+    beforeEach(() => {
+        testData = originalArr.slice(); // using data copy in our tests
+    })
+
+    it("`filterByKeys` should return an array containign elements from the original array who's keys match the given values", () => {
+
+        expect(filterByKeys(testData, ['mainID', 'subID'], {mainID:2, subID:2}))
+            .toEqual([
+                {mainID:2, subID:2, selfID:1, name:'vedran'}, // 8
+                {mainID:2, subID:2, selfID:2, name:'barbara'}, // 9
+                {mainID:2, subID:2, selfID:3, name:'patrik'}, // 10
+                {mainID:2, subID:2, selfID:4, name:'dominik'} // 11
+            ]);
+    });
+
+    it('`filterByKeys` should return an array containign elements of the original for which the comparer function has returned 0', () => {
+
+        let comparerFn = (el:any):number=> (el.mainID===2 && el.subID===2 ? 0 : 1);
+
+        expect(filterByKeys(testData, comparerFn))
+            .toEqual([
+                {mainID:2, subID:2, selfID:1, name:'vedran'}, // 8
+                {mainID:2, subID:2, selfID:2, name:'barbara'}, // 9
+                {mainID:2, subID:2, selfID:3, name:'patrik'}, // 10
+                {mainID:2, subID:2, selfID:4, name:'dominik'} // 11
+            ]);
+    });
+
+    it('`filterByKeys` should not change the order of elements in the original array', () => {
+        filterByKeys(testData, ['mainID', 'subID'], {mainID:2, subID:2});
+        expect(testData).toEqual(originalArr);
+    });
+
+    it('when `findFirstEl` kis set to TRUE, `filterByKeys` should return only the first matching element of the original array', () => {
+
+        let findFirstEl = true;
+
+        expect(filterByKeys(testData, ['mainID', 'subID'], {mainID:2, subID:2}, findFirstEl))
+            .toEqual([
+                {mainID:2, subID:2, selfID:1, name:'vedran'} // 8
+            ]);
+
+        let comparerFn = (el:any):number => el.mainID===2 && el.subID===2 ? 0 : 1;
+
+        expect(filterByKeys(testData, comparerFn, null, findFirstEl))
+            .toEqual([
+                {mainID:2, subID:2, selfID:1, name:'vedran'} // 8
+            ]);
+    });
+});
