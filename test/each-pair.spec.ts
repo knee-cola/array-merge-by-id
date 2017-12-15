1d8eb78f281e62f4513d5cc4321e361ba7ef5950
knee-cola <nikola.derezic@gmail.com>
2017-12-15 08:50:22 +0100

    test done for eachPair

--- test/each-pair.spec.ts
+++ test/each-pair.spec.ts
@@ -1,38 +1,34 @@
 import eachPair from '../lib/each-pair';
-
+import { simpleCase } from './test-data/parent-child-cases';
+import { ParentDataRow, ChildDataRow } from './test-data/test-data-types';
 
 describe('eachPair', () => {
-    it('`eachPair` treba za svaki upareni par elemenata dvaju polja pozvati callback funkciju', () => {
 
-        let callbackFn = jasmine.createSpy('callbackFn'),
-            parentsA = originalMainParentArr.slice(),
-            childenA = originalSubparentArr.slice();
+    let parentA:Array<ParentDataRow>,
+        childA:Array<ChildDataRow>;
 
-        eachPair(parentsA, childenA, ['mainID'], callbackFn);
-
-        for (let i=0, maxI=parentsA.length; i<maxI; i++) {
-
-            let parentAchildren = childenA.filter(el=>el.mainID===parentsA[i].mainID);
-
-            for (let j=0, maxJ=parentAchildren.length; j<maxJ; j++) {
-                expect(callbackFn).toHaveBeenCalledWith(parentsA[i], parentAchildren[j]);
-            }
-        }
+    beforeEach(() => {
+        // making sure the original data isn't changed
+        parentA = simpleCase.parentA.slice(),
+        childA = simpleCase.childA.slice();
     });
 
-    it('`eachPair` treba omogućiti prosljeđivanje context-a pod kojim će callback funkcija biti pozvana', () => {
+    it('`eachPair` treba za svaki upareni par elemenata dvaju polja pozvati callback funkciju', () => {
 
-        let mockContext = {},
-            callbackFn = jasmine.createSpy('callbackFn'),
-            parentsA = originalMainParentArr.slice(),
-            childenA = originalSubparentArr.slice();
+        let callbackFn = jest.fn();
 
-        eachPair(parentsA, childenA, ['mainID'], callbackFn, { context:mockContext });
+        eachPair(parentA, childA, ['FamilyID'], callbackFn);
 
-        let allCalls = callbackFn.calls.all();
+        for (let i=0, maxI=parentA.length; i<maxI; i++) {
 
-        for (let i=0, maxI=callbackFn.calls.count(); i<maxI; i++) {
-            expect(allCalls[i].object).toBe(mockContext);
+            let parent = parentA[i];
+
+            // manually fund all the children of the current parent
+            let parentChildren = childA.filter(el=>el.FamilyID===parent.FamilyID);
+
+            for (let j=0, maxJ=parentChildren.length; j<maxJ; j++) {
+                expect(callbackFn).toHaveBeenCalledWith(parent, parentChildren[j]);
+            }
         }
     });
 });
